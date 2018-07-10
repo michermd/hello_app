865072178b7fdbb6418971986dbc0041db46bca2
michermd <michermd@gmail.com>
2018-07-10 06:32:43 -0400

    minor typo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,8 @@ class ApplicationController < ActionController::Base
 
   def hello
     render html: "hola, mundo!"
-
-    link_to '/'
+    # 
+    # render hrml link_to '/goodbye'
   end
 
   def goodbye
